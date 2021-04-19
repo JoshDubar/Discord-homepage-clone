31855c56193bc057eee3a21a61eca0ebc696bc57
JoshDubar <joshuadubar123@gmail.com>
2021-04-19 19:05:45 +1000

    config update

---
+++ tailwind.config.js
@@ -0,0 +1,76 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    // Some useful comment
+    extend: {
+      maxWidth: {
+        "section-txt": "400px",
+        "screen-lg": "780px",
+        "section-final": "980px",
+        "screen-xl": "1280px",
+        footer: "1180px",
+        "footer-section": "180px",
+      },
+      minHeight: {
+        hero: "626px",
+      },
+      fontSize: {
+        "hero-title": "56px",
+        "section-title": "48px",
+        "section-mini-title": "32px",
+      },
+      padding: {
+        "section-y": "120px",
+      },
+      colors: {
+        black: "#23272A",
+        grey: "#f6f6f6",
+        blue: "#7289da",
+      },
+      inset: {
+        "1/4": "25%",
+        "1/2": "50%",
+        "3/4": "75%",
+      },
+      margin: {
+        "svg-blue-ray": "-706px",
+        "svg-big-blue-ray": "-1074px",
+        "svg-transparent-ray": "46px",
+        "svg-curve-ray": "218px",
+        "svg-left-hero": "-1300px",
+        "svg-mid-hero": "-489px",
+        "svg-right-hero": "368px",
+        7.5: "1.875rem",
+        "section-y": "120px",
+      },
+      lineHeight: {
+        section: "120%",
+        "p-text": "32px",
+      },
+      transitionProperty: {
+        section: "margin, padding, opacity",
+      },
+      keyframes: {
+        "ease-up": {
+          "0%": {
+            marginTop: "40px",
+            opacity: "0",
+          },
+          "100%": {
+            marginTop: "0px",
+            opacity: "1",
+          },
+        },
+      },
+    },
+    animation: {
+      "ease-up": "ease-up 0.5s ease",
+    },
+  },
+  variants: {
+    // Some useful comment
+  },
+  plugins: [
+    // Some useful comment
+  ],
+};
